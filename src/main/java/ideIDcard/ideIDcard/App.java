c8ceeecb9a4e8194df6d097f7c5a03d33bac35c1
Raymart De Guzman <tramyardg@gmail.com>
2018-12-31 12:30:17 -0500

    post gitignore changes

---
+++ src/main/java/ideIDcard/ideIDcard/App.java
@@ -0,0 +1,13 @@
+package ideIDcard.ideIDcard;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
