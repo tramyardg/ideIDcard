ab137f9a5d7f934796b0c0613b9b3de7533ad365
Raymart De Guzman <tramyardg@gmail.com>
2018-12-31 13:12:07 -0500

    file menu bar & exit

--- src/main/java/ideIDcard/AppMainWindow.java
+++ src/main/java/ideIDcard/AppMainWindow.java
@@ -1,6 +1,9 @@
 package ideIDcard;
 
+import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Menu;
+import org.eclipse.swt.widgets.MenuItem;
 import org.eclipse.swt.widgets.Shell;
 
 public class AppMainWindow {
@@ -9,6 +12,7 @@ public class AppMainWindow {
 
     /**
      * Launch the application.
+     * 
      * @param args
      */
     public static void main(String[] args) {
@@ -24,10 +28,25 @@ public class AppMainWindow {
      * Open the window.
      */
     public void open() {
-	Display display = Display.getDefault();
-	createContents();
-	shell.open();
-	shell.layout();
+	Display display = new Display();
+	Shell shell = new Shell(display, SWT.SHELL_TRIM | SWT.CENTER);
+
+	Menu menuBar = new Menu(shell, SWT.BAR);
+	MenuItem cascadeFileMenu = new MenuItem(menuBar, SWT.CASCADE);
+	cascadeFileMenu.setText("&File");
+
+	Menu fileMenu = new Menu(shell, SWT.DROP_DOWN);
+	cascadeFileMenu.setMenu(fileMenu);
+	MenuItem exitItem = new MenuItem(fileMenu, SWT.PUSH);
+	exitItem.setText("&Exit");
+
+	shell.setMenuBar(menuBar);
+	exitItem.addListener(SWT.Selection, event -> {
+	    shell.getDisplay().dispose();
+	    System.exit(0);
+	});
+
+	createContents(shell);
 	while (!shell.isDisposed()) {
 	    if (!display.readAndDispatch()) {
 		display.sleep();
@@ -38,11 +57,11 @@ public class AppMainWindow {
     /**
      * Create contents of the window.
      */
-    protected void createContents() {
-	shell = new Shell();
-	shell.setSize(450, 300);
-	shell.setText("SWT Application");
-
+    protected void createContents(Shell shell) {
+	shell.setSize(960, 500);
+	shell.setText("Image Data Extraction Application");
+	shell.open();
+	shell.layout();
     }
 
 }
